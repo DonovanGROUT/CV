1ddc9249a183252f6e85f43d7fbcddb62e3a8dd8
DonovanGROUT <donovan.grout.pro@gmail.com>
2024-08-27 17:03:14 +0200

    Debug bouton mode clair/mode sombre

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,9 @@ if (currentTheme === 'dark') {
     document.documentElement.setAttribute('data-theme', 'dark');
     toggleButton.classList.remove('bi-moon-stars-fill');
     toggleButton.classList.add('bi-sun-fill');
-    toggleButton.textContent = '';
+    toggleButton.textContent = '\u00A0'; // Remplacer le texte par un espace insécable
+} else {
+    toggleButton.textContent = '\u00A0'; // Remplacer le texte par un espace insécable
 }
 
     // Gestion du clic sur le bouton
@@ -33,15 +35,17 @@ toggleButton.addEventListener('click', () => {
 
     document.documentElement.setAttribute('data-theme', isDark ? 'light' : 'dark');
     localStorage.setItem('theme', isDark ? 'light' : 'dark');
-    toggleButton.textContent = '';
+    toggleButton.textContent = '\u00A0';
 
     // Changer l'icône selon le thème
     if (isDark) {
         toggleButton.classList.remove('bi-sun-fill');
         toggleButton.classList.add('bi-moon-stars-fill');
+        toggleButton.setAttribute('aria-label', 'Passer en mode clair');
     } else {
         toggleButton.classList.remove('bi-moon-stars-fill');
         toggleButton.classList.add('bi-sun-fill');
+        toggleButton.setAttribute('aria-label', 'Passer en mode sombre');
     }
 });
 
