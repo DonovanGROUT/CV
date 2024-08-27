a235370d53d9f80dfe6843ad3930c61ca204a041
DonovanGROUT <donovan.grout.pro@gmail.com>
2024-08-27 11:39:34 +0200

    Debug affichage background Iphone

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
     const toggleButton = document.getElementById('theme-toggle');
     const currentTheme = localStorage.getItem('theme') || 'light';
+    var isIOS = /iPad|iPhone|iPod/.test(navigator.platform) || 
+    (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
+
+    if (isIOS) {
+    document.body.style.backgroundImage = 'url("../images/wallpaper.webp")';
+    }
 
 
     // Mode sombre
